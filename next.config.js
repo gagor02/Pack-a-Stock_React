0acb4fa1547812eb39dc08b6fef5eee85ad37156
Ernesto Garcia Valenzuela <0322103721@ut-tijuana.edu.mx>
2026-04-09 23:34:37 -0700

    xd

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   outputFileTracingRoot: __dirname,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com",
+              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com",
+              "connect-src 'self' https://apis.google.com https://accounts.google.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://packstock.198.71.54.179.nip.io",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https://packstock.198.71.54.179.nip.io https://lh3.googleusercontent.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
